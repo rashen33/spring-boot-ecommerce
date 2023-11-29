fbb925063af0479c871447a5047de77a4678d4ee
Rashen <rashen598@gmail.com>
2023-11-29 12:34:36 +0530

    added cross origin support for the repos

--- src/main/java/edu/personal/repository/ProductCategoryRepository.java
+++ src/main/java/edu/personal/repository/ProductCategoryRepository.java
@@ -3,6 +3,9 @@ package edu.personal.repository;
 import edu.personal.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200") //Accept calls from web browser scripts for the origin
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") //name of the json entry and /product-category
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- src/main/java/edu/personal/repository/ProductRepository.java
+++ src/main/java/edu/personal/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package edu.personal.repository;
 
 import edu.personal.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") //Accept calls from web browser scripts for the origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
