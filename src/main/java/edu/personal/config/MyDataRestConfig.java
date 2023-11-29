4412ed7f5e3c16816d084a9738e293f0ece82904
Rashen <rashen598@gmail.com>
2023-11-29 11:19:58 +0530

    made product and product categories readonly

--- src/main/java/edu/personal/config/MyDataRestConfig.java
+++ src/main/java/edu/personal/config/MyDataRestConfig.java
@@ -1,10 +1,14 @@
 package edu.personal.config;
 
+import edu.personal.entity.Product;
+import edu.personal.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -14,7 +18,13 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         //disabling HTTP methods for Product : PUT, POST and DELETE
         config.getExposureConfiguration()
-                .forDomainType(Process.class)
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        //disabling HTTP methods for ProductCategory : PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
     }
